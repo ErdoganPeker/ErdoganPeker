a0aa35efb1fd36b4af081eb6f7f6adbf96df985d
Erdogan Peker <111955856+ErdoganPeker@users.noreply.github.com>
2023-01-25 23:19:25 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+int i=5;
+int *p=&i;
+printf("%p",p);
+// & degiskenin adres bilgisini veriyor
+// 0 
+	
+	
+	
+	return 0;
+}
